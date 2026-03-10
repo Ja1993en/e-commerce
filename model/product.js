cb76a90380352c5281c133a58a498cbeaf5cfb8c
Basquiat <jlnmcneal@gmail.com>
2026-03-10 18:12:47 -0500

    Sequelize model for the product table

---
+++ model/product.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes, INTEGER } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Product extends Model {};
+
+Product.init(
+    {
+       id: {
+           type: DataTypes.INTEGER,
+           autoIncrement: true,
+           primaryKey: true,
+           allowNull: false,
+        },
+        product_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                isDecimal: true
+            }
+        },
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: "10",
+            validate: {
+                isNumeric: true
+            }
+        },
+         category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+            model: "category",
+            key: "id"
+            },
+
+        },
+        
+    },
+
+    {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Product',
+  }
+);
+
+
+module.exports = Product;
