fbb702ae54485e1d71b00befd72d1071a79f4af6
Basquiat <jlnmcneal@gmail.com>
2026-03-11 00:38:24 -0500

    Seed file for the category table

---
+++ seeds/category-seeds.js
@@ -0,0 +1,19 @@
+const categoryData = [
+    {
+        category_name: 'Shirts',
+    },
+    {
+        category_name: 'Shorts',
+    },
+    {
+        category_name: 'Music',
+    },
+    {
+        category_name: 'Hats',
+    },
+    {
+        category_name: 'Shoes',
+    }
+]
+
+module.exports = categoryData;
